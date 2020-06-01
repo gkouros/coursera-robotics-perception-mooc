f48fb003c5ebdad086047c91c8a02939b6211d90
George Kouros <george@kapernikov.com>
2020-06-01 11:59:52 +0200

    Add completed homography estimation function

--- assignments/Week2 - Computer Projective Transformations/RoboticsPerceptionWeek2AssignmentCode/est_homography.m
+++ assignments/Week2 - Computer Projective Transformations/RoboticsPerceptionWeek2AssignmentCode/est_homography.m
@@ -9,7 +9,25 @@ function [ H ] = est_homography(video_pts, logo_pts)
 % Written for the University of Pennsylvania's Robotics:Perception course
 
 % YOUR CODE HERE
-H = [];
+
+% create lambda functions that generate vectors ax and ay for each point
+ax = @(x, xp)[-x(1), - x(2), -1, 0, 0, 0, x(1)*xp(1), x(2)*xp(1), xp(1)];
+ay = @(x, xp)[0, 0, 0, -x(1), -x(2), -1, x(1)*xp(2), x(2)*xp(2), xp(2)];
+
+% get number of points (should be 4)
+len = length(video_pts);
+assert(len == 4);
+
+% initialize matrix A with zeros to prevent resizing in loop
+A = zeros(len, 9);
+
+for i=1:len
+    A(i * 2 - 1, :) = ax(video_pts(i, :), logo_pts(i, :));
+    A(i * 2, :) = ay(video_pts(i, :), logo_pts(i, :));
+end
+    
+[U, S, V] = svd(A);
+H = reshape(V(:, end), [3,3])';
 
 end
 
